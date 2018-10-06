fb74c35dc19a52247f22869d69f7ec44da2316ad
Éric Massé <Ericmas001@gmail.com>
2018-10-06 18:21:06 -0400

    Using Common 0.5.1

--- SimplyAnIcon.Samples.NotifyIcon/Services/IconLogicService.cs
+++ SimplyAnIcon.Samples.NotifyIcon/Services/IconLogicService.cs
@@ -10,40 +10,29 @@ using SimplyAnIcon.Common.Models;
 using SimplyAnIcon.Samples.NotifyIcon.Helpers;
 using SimplyAnIcon.Samples.NotifyIcon.Services.Interfaces;
 using SimplyAnIcon.Common.Services.Interfaces;
-using SimplyAnIcon.Common.Settings.Interface;
-using SimplyAnIcon.Plugins.V1;
-using SimplyAnIcon.Plugins.Wpf.V1;
 using SimplyAnIcon.Plugins.Wpf.V1.MenuItemViewModels;
 
 namespace SimplyAnIcon.Samples.NotifyIcon.Services
 {
     public class IconLogicService : IIconLogicService
     {
-        private readonly List<ISimplyAWpfPlugin> _pluginWpfInstances = new List<ISimplyAWpfPlugin>();
-        private readonly List<ISimplyAPlugin> _pluginInstances = new List<ISimplyAPlugin>();
         private readonly IPluginService _pluginService;
-        private readonly IPluginSettings _pluginSettings;
 
-        public PluginCatalog PluginsCatalog => new PluginCatalog
-        {
-            PluginInstances = _pluginInstances,
-            PluginWpfInstances = _pluginWpfInstances
-        };
-        
+        public PluginCatalog PluginsCatalog { get; private set; }
+
         public event EventHandler<IEnumerable<MenuItemViewModel>> OnMenuBuilt = delegate { };
         public event EventHandler OnAppExited = delegate { };
 
-        public IconLogicService(IPluginService pluginService, IPluginSettings pluginSettings)
+        public IconLogicService(IPluginService pluginService)
         {
             _pluginService = pluginService;
-            _pluginSettings = pluginSettings;
         }
 
         public void UpdateIcon()
         {
             LoadPlugins();
-            _pluginInstances.ForEach(x => x.OnRefresh());
-            _pluginWpfInstances.ForEach(x => x.OnRefresh());
+            PluginsCatalog.ActiveBackgroungPlugins.ToList().ForEach(x => x.OnRefresh());
+            PluginsCatalog.ActiveForegroundPlugins.ToList().ForEach(x => x.OnRefresh());
             OnMenuBuilt(this, BuildMenu());
             BuildMenu();
         }
@@ -55,40 +44,27 @@ namespace SimplyAnIcon.Samples.NotifyIcon.Services
 
         public void OnDispose()
         {
-            _pluginInstances.ForEach(x => x.OnDispose());
-            _pluginWpfInstances.ForEach(x => x.OnDispose());
+            PluginsCatalog.ActiveBackgroungPlugins.ToList().ForEach(x => x.OnDispose());
+            PluginsCatalog.ActiveForegroundPlugins.ToList().ForEach(x => x.OnDispose());
         }
         private void LoadPlugins()
         {
             var registrantBuilder = new RegistrantFinderBuilder()
                 .AddAssemblyPrefix("IconeIA.Core");
 
-            var plugins = _pluginService.LoadPlugins(Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), "Plugins"), new UnityInstanceResolverHelper(), registrantBuilder);
-
-            var settings = _pluginSettings.GetPlugins().ToArray();
-            foreach (var plugin in plugins.PluginInstances.Concat(plugins.PluginWpfInstances))
-            {
-                if (!settings.Any(x => x.Name.Equals(plugin.Name, StringComparison.InvariantCultureIgnoreCase)))
-                    _pluginSettings.AddPlugin(plugin.Name);
-            }
-
-            _pluginInstances.Clear();
-            _pluginInstances.AddRange(plugins.PluginInstances);
-
-            _pluginWpfInstances.Clear();
-            _pluginWpfInstances.AddRange(plugins.PluginWpfInstances);
-
-            foreach (var resourceDictionary in _pluginWpfInstances.SelectMany(x => x.ResourceDictionaries))
+            PluginsCatalog = _pluginService.LoadPlugins(Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), "Plugins"), new UnityInstanceResolverHelper(), registrantBuilder);
+            
+            foreach (var resourceDictionary in PluginsCatalog.ActiveForegroundPlugins.SelectMany(x => x.ResourceDictionaries))
                 Application.Current.Resources.MergedDictionaries.Add(resourceDictionary);
 
-            _pluginInstances.ForEach(x => x.OnInit());
-            _pluginWpfInstances.ForEach(x => x.OnInit());
+            PluginsCatalog.ActiveBackgroungPlugins.ToList().ForEach(x => x.OnInit());
+            PluginsCatalog.ActiveForegroundPlugins.ToList().ForEach(x => x.OnInit());
         }
 
         private IEnumerable<MenuItemViewModel> BuildMenu()
         {
             var items = new List<MenuItemViewModel>();
-            var itemsOfPlugin = _pluginWpfInstances.Select(x => x.MenuItems).ToList();
+            var itemsOfPlugin = PluginsCatalog.ActiveForegroundPlugins.Select(x => x.MenuItems).ToList();
             var first = true;
             foreach (var it in itemsOfPlugin)
             {
